97775c86ba3ec52d7c8e1b5b66c106a85fb7232b
Suhad Al-Othman <sohadosman15@gmail.com>
2025-08-29 16:31:20 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  >
       <AuthProvider>
         <App />
       </AuthProvider>
